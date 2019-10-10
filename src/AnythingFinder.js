f0ac090bb43f2c154af7ed2eddba6c557c1d880f
Eduardo Aguilar <eduardo.aguilar@soprasteria.com>
2019-10-10 11:09:22 +0200

    refactor: removed unnecessary property

--- src/AnythingFinder.js
+++ src/AnythingFinder.js
@@ -13,24 +13,17 @@ export class AnythingFinder extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      counter: { type: Number },
     };
   }
 
   constructor() {
     super();
     this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <h2>${this.title}</h2>
     `;
   }
 }
