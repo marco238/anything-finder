5ae06897a9364bc816df7cd7b50cfc1e3019a8a5
Marco <marco238cuba@gmail.com>
2019-10-22 15:43:16 +0200

    feat: deploy to gh-pages available

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
 import resolve from 'rollup-plugin-node-resolve';
 import common from 'rollup-plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'lib/AnythingFinder.js',
   output: {
-    file: 'build/bundle.js',
+    file: 'dist/src/AnythingFinder.js',
     format: 'esm'
   },
   plugins: [
     common(),
-    resolve()
+    resolve(),
+    copy({
+      targets: [
+        { 
+          src: 'index.html',
+          dest: 'dist'
+        }
+      ]
+    })
   ]
 };
